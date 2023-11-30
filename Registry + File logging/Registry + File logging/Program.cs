ba08158526fe5255e3718f28d8a8c60fbf259997
Thr6Defolt <43175754+Thr6Defolt@users.noreply.github.com>
2023-11-30 10:19:15 +0200

    Add files via upload

---
+++ Registry + File logging/Registry + File logging/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace Registry___File_logging
+{
+    internal class Program
+    {
+        public const string directoryPath = @"C:\test";
+        public const string fileName = "Sample.txt";
+        public static string filePath = Path.Combine(directoryPath, fileName);
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo myDirectory = new DirectoryInfo(directoryPath);
+            FileInfo myFile = new FileInfo(filePath);
+            if (!myDirectory.Exists)
+            {
+                myDirectory.Create();
+                Console.WriteLine("Directory created successfully.");
+            }
+
+            if (!myFile.Exists)
+            {
+                using (File.Create(filePath))
+                {
+                    Console.WriteLine("File created successfully.");
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine(DateTime.Now);
+                }
+                Console.WriteLine("File already exists.");
+            }
+        }
+    }
+}
